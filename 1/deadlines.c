5a6fb6c1e5f8ce6c6b37e14f814d3fce6eae460f
Rui <dxdeidara@gmail.com>
2017-02-19 15:30:24 -0800

    res

--- 1/deadlines.c
+++ 1/deadlines.c
@@ -1,12 +1,12 @@
 // Generated deadlines for the test workload
 // This file has to be included only in main_week4.c
 long long workloadDeadlines[] = {
-	20000,
-	50000,
-	60000,
-	40000,
-	60000,
-	60000,
-	30000,
-	15000,
+	200000,
+	500000,
+	600000,
+	400000,
+	600000,
+	600000,
+	300000,
+	150000,
 };
