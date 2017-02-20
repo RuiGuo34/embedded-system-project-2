bca4284cd67914902eef4d095ca4daeaa09b5652
Rui <dxdeidara@gmail.com>
2017-02-19 20:20:43 -0800

    final version

--- assignment2.c
+++ assignment2.c
@@ -262,7 +262,7 @@ TaskSelection select_task(SharedVariable* sv, const int* aliveTasks, long long i
 
 	prev_freq = res.freq;
 
-	energy = energy + ((float)idleTime/1000000)*50 + ((float)time_difference/1000000)*CPU_WORK[prev_freq];
+	energy = energy + ((float)idleTime)*50 + ((float)time_difference)*CPU_WORK[prev_freq];
 	// printDBG("Energy: %lld\n", energy);
 	// printDBG("Time Difference %lld\n", time_difference);
 	// printTasks(aliveTasks);
